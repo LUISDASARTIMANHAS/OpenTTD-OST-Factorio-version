b635e9b7331957a7871e1b76f08f78b5b1e411b1
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2023-12-19 20:26:40 -0300

    updated base

--- data.lua
+++ data.lua
@@ -100,4 +100,193 @@ data:extend({
             filename = path_main .. "audio/flying_scotsman.mid"
         }
     },
+    {
+        type = "ambient-sound",
+        name = "harp_harmony",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/harp_harmony.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "keep_on_rolling",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/keep_on_rolling.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "linns_basket",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/linns_basket.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "midnight_snow_run",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/midnight_snow_run.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "mighty_giant_run",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/mighty_giant_run.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "modern_motion",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/modern_motion.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "moo_redfarn",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/moo_redfarn.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "mosey_along_redfarn",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/mosey_along_redfarn.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "no_work_song_redfarn",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/no_work_song_redfarn.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "relax_song",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/relax_song.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "run_for_your_life",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/run_for_your_life.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "say_what_redfarn",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/say_what_redfarn.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "slow_neasy_redfarn",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/slow_neasy_redfarn.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "the_fast_route",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/the_fast_route.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "the_hobo_redfarn",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/the_hobo_redfarn.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "train_filled_with_cash",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/train_filled_with_cash.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "ttsong_iii_imuh3",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/ttsong_iii_imuh3.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "ttsong_iv_imuh3",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/ttsong_iv_imuh3.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "tttheme2",
+        track_type = "menu-track",
+        sound =
+        {
+            filename = path_main .. "audio/tttheme2.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "ultimate_run",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/ultimate_run.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "wood_whistles",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/wood_whistles.mid"
+        }
+    }
 })
