7fb01c8052702e1f8942c36358d8f985329d0e72
LUIS_DAS_ARTIMANHAS <75493473+LUISDASARTIMANHAS@users.noreply.github.com>
2023-12-19 19:28:56 -0300

    Add files via upload

---
+++ data.lua
@@ -0,0 +1,103 @@
+local path_main = "__OpenTTD-OST__/"
+
+data:extend({
+    {
+        type = "ambient-sound",
+        name = "5432gone_redfarn",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/5432gone_redfarn.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "be_sharp_bw_redfarn",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/be_sharp_bw_redfarn.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "boogi_marabi_redfarn",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/boogi_marabi_redfarn.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "busy_schedule",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/busy_schedule.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "careless_perc_redfarn",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/careless_perc_redfarn.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "5432gone_redfarn",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/5432gone_redfarn.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "chemistry_lab",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/chemistry_lab.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "chuggachugga",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/chuggachugga.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "city_blues_redfarn",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/city_blues_redfarn.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "coconut_run2",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/coconut_run2.mid"
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "flying_scotsman",
+        track_type = "main-track",
+        sound =
+        {
+            filename = path_main .. "audio/flying_scotsman.mid"
+        }
+    },
+})
