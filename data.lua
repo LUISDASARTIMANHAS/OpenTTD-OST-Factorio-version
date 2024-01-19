73710158824fc6625e96a678c359faacaea1c9af
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2024-01-18 22:54:18 -0300

    updated

--- data.lua
+++ data.lua
@@ -1,292 +1,329 @@
 local path_main = "__OpenTTD-OST__/audio/"
+-- local ambientMusic = open_ttd_ambient_music_config.value
+-- local menuMusic = open_ttd_menu_music_config.value
 
 data:extend({
     {
         type = "ambient-sound",
         name = "5432gone_redfarn",
-        track_type = "main-track",
+        track_type = "interlude",
         sound =
         {
-            filename = path_main .. "5432gone_redfarn.mp3"
+            filename = path_main .. "5432gone_redfarn.mp3",
+            volume = 1.2
         }
     },
     {
         type = "ambient-sound",
         name = "be_sharp_bw_redfarn",
-        track_type = "main-track",
+        track_type = "interlude",
         sound =
         {
-            filename = path_main .. "be_sharp_bw_redfarn.mp3"
+            filename = path_main .. "be_sharp_bw_redfarn.mp3",
+            volume = 1.2
         }
     },
     {
         type = "ambient-sound",
         name = "boogi_marabi_redfarn",
-        track_type = "main-track",
+        track_type = "interlude",
         sound =
         {
-            filename = path_main .. "boogi_marabi_redfarn.mp3"
+            filename = path_main .. "boogi_marabi_redfarn.mp3",
+            volume = 1.2
         }
     },
     {
         type = "ambient-sound",
         name = "busy_schedule",
-        track_type = "main-track",
+        track_type = "interlude",
         sound =
         {
-            filename = path_main .. "busy_schedule.mp3"
+            filename = path_main .. "busy_schedule.mp3",
+            volume = 1.2
         }
     },
     {
         type = "ambient-sound",
         name = "careless_perc_redfarn",
-        track_type = "main-track",
+        track_type = "interlude",
         sound =
         {
-            filename = path_main .. "careless_perc_redfarn.mp3"
+            filename = path_main .. "careless_perc_redfarn.mp3",
+            volume = 1.2
         }
     },
     {
         type = "ambient-sound",
         name = "5432gone_redfarn",
-        track_type = "main-track",
+        track_type = "interlude",
         sound =
         {
-            filename = path_main .. "5432gone_redfarn.mp3"
+            filename = path_main .. "5432gone_redfarn.mp3",
+            volume = 1.2
         }
     },
     {
         type = "ambient-sound",
         name = "chemistry_lab",
-        track_type = "main-track",
+        track_type = "interlude",
         sound =
         {
-            filename = path_main .. "chemistry_lab.mp3"
+            filename = path_main .. "chemistry_lab.mp3",
+            volume = 1.2
         }
     },
     {
         type = "ambient-sound",
         name = "chuggachugga",
-        track_type = "main-track",
+        track_type = "interlude",
         sound =
         {
-            filename = path_main .. "chuggachugga.mp3"
+            filename = path_main .. "chuggachugga.mp3",
+            volume = 1.2
         }
     },
     {
         type = "ambient-sound",
         name = "city_blues_redfarn",
-        track_type = "main-track",
+        track_type = "interlude",
         sound =
         {
-            filename = path_main .. "city_blues_redfarn.mp3"
+            filename = path_main .. "city_blues_redfarn.mp3",
+            volume = 1.2
         }
     },
     {
         type = "ambient-sound",
         name = "coconut_run2",
-        track_type = "main-track",
+        track_type = "interlude",
         sound =
         {
-            filename = path_main .. "coconut_run2.mp3"
+            filename = path_main .. "coconut_run2.mp3",
+            volume = 1.2
         }
     },
     {
         type = "ambient-sound",
         name = "flying_scotsman",
-        track_type = "main-track",
+        track_type = "interlude",
         sound =
         {
-            filename = path_main .. "flying_scotsman.mp3"
+            filename = path_main .. "flying_scotsman.mp3",
+            volume = 1.2
         }
     },
     {
         type = "ambient-sound",
         name = "harp_harmony",
-        track_type = "main-track",
+        track_type = "interlude",
         sound =
         {
-            filename = path_main .. "harp_harmony.mp3"
+            filename = path_main .. "harp_harmony.mp3",
+            volume = 1.2
         }
     },
     {
         type = "ambient-sound",
         name = "keep_on_rolling",
-        track_type = "main-track",
+        track_type = "interlude",
         sound =
         {
-            filename = path_main .. "keep_on_rolling.mp3"
+            filename = path_main .. "keep_on_rolling.mp3",
+            volume = 1.2
         }
     },
     {
         type = "ambient-sound",
         name = "linns_basket",
-        track_type = "main-track",
+        track_type = "interlude",
         sound =
         {
-            filename = path_main .. "linns_basket.mp3"
+            filename = path_main .. "linns_basket.mp3",
+            volume = 1.2
         }
     },
     {
         type = "ambient-sound",
         name = "midnight_snow_run",
-        track_type = "main-track",
+        track_type = "interlude",
         sound =
         {
-            filename = path_main .. "midnight_snow_run.mp3"
+            filename = path_main .. "midnight_snow_run.mp3",
+            volume = 1.2
         }
     },
     {
         type = "ambient-sound",
         name = "mighty_giant_run",
-        track_type = "main-track",
+        track_type = "interlude",
         sound =
         {
-            filename = path_main .. "mighty_giant_run.mp3"
+            filename = path_main .. "mighty_giant_run.mp3",
+            volume = 1.2
         }
     },
     {
         type = "ambient-sound",
         name = "modern_motion",
-        track_type = "main-track",
+        track_type = "interlude",
         sound =
         {
-            filename = path_main .. "modern_motion.mp3"
+            filename = path_main .. "modern_motion.mp3",
+            volume = 1.2
         }
     },
     {
         type = "ambient-sound",
         name = "moo_redfarn",
-        track_type = "main-track",
+        track_type = "interlude",
         sound =
         {
-            filename = path_main .. "moo_redfarn.mp3"
+            filename = path_main .. "moo_redfarn.mp3",
+            volume = 1.2
         }
     },
     {
         type = "ambient-sound",
         name = "mosey_along_redfarn",
-        track_type = "main-track",
+        track_type = "interlude",
         sound =
         {
-            filename = path_main .. "mosey_along_redfarn.mp3"
+            filename = path_main .. "mosey_along_redfarn.mp3",
+            volume = 1.2
         }
     },
     {
         type = "ambient-sound",
         name = "no_work_song_redfarn",
-        track_type = "main-track",
+        track_type = "interlude",
         sound =
         {
-            filename = path_main .. "no_work_song_redfarn.mp3"
+            filename = path_main .. "no_work_song_redfarn.mp3",
+            volume = 1.2
         }
     },
     {
         type = "ambient-sound",
         name = "relax_song",
-        track_type = "main-track",
+        track_type = "interlude",
         sound =
         {
-            filename = path_main .. "relax_song.mp3"
+            filename = path_main .. "relax_song.mp3",
+            volume = 1.2
         }
     },
     {
         type = "ambient-sound",
         name = "run_for_your_life",
-        track_type = "main-track",
+        track_type = "interlude",
         sound =
         {
-            filename = path_main .. "run_for_your_life.mp3"
+            filename = path_main .. "run_for_your_life.mp3",
+            volume = 1.2
         }
     },
     {
         type = "ambient-sound",
         name = "say_what_redfarn",
-        track_type = "main-track",
+        track_type = "interlude",
         sound =
         {
-            filename = path_main .. "say_what_redfarn.mp3"
+            filename = path_main .. "say_what_redfarn.mp3",
+            volume = 1.2
         }
     },
     {
         type = "ambient-sound",
         name = "slow_neasy_redfarn",
-        track_type = "main-track",
+        track_type = "interlude",
         sound =
         {
-            filename = path_main .. "slow_neasy_redfarn.mp3"
+            filename = path_main .. "slow_neasy_redfarn.mp3",
+            volume = 1.2
         }
     },
     {
         type = "ambient-sound",
         name = "the_fast_route",
-        track_type = "main-track",
+        track_type = "interlude",
         sound =
         {
-            filename = path_main .. "the_fast_route.mp3"
+            filename = path_main .. "the_fast_route.mp3",
+            volume = 1.2
         }
     },
     {
         type = "ambient-sound",
         name = "the_hobo_redfarn",
-        track_type = "main-track",
+        track_type = "interlude",
         sound =
         {
-            filename = path_main .. "the_hobo_redfarn.mp3"
+            filename = path_main .. "the_hobo_redfarn.mp3",
+            volume = 1.2
         }
     },
     {
         type = "ambient-sound",
         name = "train_filled_with_cash",
-        track_type = "main-track",
+        track_type = "interlude",
         sound =
         {
-            filename = path_main .. "train_filled_with_cash.mp3"
+            filename = path_main .. "train_filled_with_cash.mp3",
+            volume = 1.2
         }
     },
     {
         type = "ambient-sound",
         name = "ttsong_iii_imuh3",
-        track_type = "main-track",
+        track_type = "interlude",
         sound =
         {
-            filename = path_main .. "ttsong_iii_imuh3.mp3"
+            filename = path_main .. "ttsong_iii_imuh3.mp3",
+            volume = 1.2
         }
     },
     {
         type = "ambient-sound",
         name = "ttsong_iv_imuh3",
-        track_type = "main-track",
+        track_type = "interlude",
         sound =
         {
-            filename = path_main .. "ttsong_iv_imuh3.mp3"
+            filename = path_main .. "ttsong_iv_imuh3.mp3",
+            volume = 1.2
         }
     },
+    {
+        type = "ambient-sound",
+        name = "ultimate_run",
+        track_type = "interlude",
+        sound =
+        {
+            filename = path_main .. "ultimate_run.mp3",
+            volume = 1.2
+        }
+    },
+    {
+        type = "ambient-sound",
+        name = "wood_whistles",
+        track_type = "interlude",
+        sound =
+        {
+            filename = path_main .. "wood_whistles.mp3",
+            volume = 1.2
+        }
+    }
+})
+
+data:extend({
     {
         type = "ambient-sound",
         name = "tttheme2",
         track_type = "menu-track",
         sound =
         {
-            filename = path_main .. "tttheme2.mp3"
-        }
-    },
-    {
-        type = "ambient-sound",
-        name = "ultimate_run",
-        track_type = "main-track",
-        sound =
-        {
-            filename = path_main .. "ultimate_run.mp3"
-        }
-    },
-    {
-        type = "ambient-sound",
-        name = "wood_whistles",
-        track_type = "main-track",
-        sound =
-        {
-            filename = path_main .. "wood_whistles.mp3"
+            filename = path_main .. "tttheme2.mp3",
+            volume = 1.2
         }
     }
 })
---
+++ settings.lua
@@ -0,0 +1,16 @@
+data:extend({
+    {
+        type = "bool-setting",
+        name = "open_ttd_ambient_music_config",
+        setting_type = "startup",
+        localised_name = "enable ambient music",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "open_ttd_music_menu_config",
+        setting_type = "startup",
+        localised_name = "enable menu music",
+        default_value = true
+    }
+})
